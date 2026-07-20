06a56da53ca23ac0a6ba097015c1ef5541ce26d5
yanelidiaz93 <yanelidiaz93@gmail.com>
2026-07-20 10:29:11 -0600

    Aprendiendo JS (imprimir, variables)

---
+++ JavaScript/00-HelloWorld.js
@@ -0,0 +1,17 @@
+//Comentario simple
+
+/*
+Comentario en
+varias
+lineas
+ */
+
+console.log("Hello World, JavaScript!")
+console.log('Hola')
+console.log(`JavaScript`)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
---
+++ JavaScript/01-Variables.js
@@ -0,0 +1,23 @@
+// var
+
+var helloWorld = "Hola, JavaScript"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo";
+console.log(helloWorld);
+
+//let ----> Cuando los valores varian
+
+let helloWorld2 = "Hola, JavaScript 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo 2";
+console.log(helloWorld2);
+
+//const ----> cuando los valores no varian
+
+const helloWorld3 = "Hola, JavaScript 3"
+console.log(helloWorld3)
+//Por lo que esto no se puede hacer:
+/*helloWorld3 = "Hola de nuevo 3";
+console.log(helloWorld3);*/
